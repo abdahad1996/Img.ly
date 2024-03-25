3c1b9de2a3240532c959c22dba76cc9a28eaeb58
abdul ahad <abdul_ahad1996@live.com>
2024-03-25 20:31:43 +0100

    - delete unused tests - added http client spy - added memory leak helper

---
+++ IMGLY/CoreTests/Api/RemoteTreeNodeLoaderTests.swift
@@ -0,0 +1,78 @@
+//
+//  RemoteTreeNodeLoader.swift
+//  CoreTests
+//
+//  Created by macbook abdul on 25/03/2024.
+//
+
+import Foundation
+import XCTest
+import Core
+
+
+public protocol HTTPClient {
+    func get(from url: URL) async throws -> (Data, HTTPURLResponse)
+}
+
+class RemoteTreeNodeLoader:TreeNodeLoader {
+    
+    let url:URL
+    let client:HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    func load() async throws -> [TreeNode] {
+        throw Error.connectivity
+    }
+    
+    
+}
+class RemoteTreeNodeLoaderTests:XCTestCase{
+    
+    
+    
+    
+    // MARK: - Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, result: (Data, HTTPURLResponse)? = nil, error: RemoteTreeNodeLoader.Error? = nil, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteTreeNodeLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy(result: result, error: error)
+        let sut = RemoteTreeNodeLoader(url: url, client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        private(set) var requests = [URL]()
+        private let result: (Data, HTTPURLResponse)?
+        private let error: RemoteTreeNodeLoader.Error?
+
+        var requestedURLs: [URL] {
+            return requests
+        }
+
+        init(result: (Data, HTTPURLResponse)?, error: RemoteTreeNodeLoader.Error?) {
+            self.result = result
+            self.error = error
+        }
+
+        func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
+            self.requests.append(url)
+            guard let result = result else {
+                throw error ?? .connectivity
+            }
+
+            return result
+        }
+    }
+}
+
+
---
+++ IMGLY/CoreTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  CoreTests
+//
+//  Created by macbook abdul on 25/03/2024.
+//
+
+import Foundation
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
