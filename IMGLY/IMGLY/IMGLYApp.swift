2ff8fe552caee7a4a15a126c991cf0f8417c1090
abdul ahad <abdul_ahad1996@live.com>
2024-03-25 16:51:43 +0100

    - create a new project

---
+++ IMGLY/IMGLY/IMGLYApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMGLYApp.swift
+//  IMGLY
+//
+//  Created by macbook abdul on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMGLYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
