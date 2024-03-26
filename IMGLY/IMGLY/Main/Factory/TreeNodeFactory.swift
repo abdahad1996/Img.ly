0f8e611a364b96202e86e7a75e41555a841cc86c
abdul ahad <abdul_ahad1996@live.com>
2024-03-26 01:43:47 +0100

    - add factory for encapsulating creation logic for remoteTreeNodeLoader

---
+++ IMGLY/IMGLY/Main/Factory/TreeNodeFactory.swift
@@ -0,0 +1,18 @@
+//
+//  RootFactory.swift
+//  SwiftHeroes
+//
+//  Created by macbook abdul on 23/03/2024.
+//
+
+import Foundation
+import Core
+
+final class TreeNodeFactory {
+	static func treeLoader(baseURL: URL) -> RemoteTreeNodeLoader {
+		let session = URLSession(configuration: .ephemeral)
+		let httpClient = URLSessionHTTPAdapter(session: session)
+		let remoteTreeLoader = RemoteTreeNodeLoader(url: baseURL, client: httpClient)
+		return remoteTreeLoader
+	}
+}
