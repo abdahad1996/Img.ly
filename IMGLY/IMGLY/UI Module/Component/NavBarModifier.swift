c370f8501044013777e0378d16f4a4c7b28399da
abdul ahad <abdul_ahad1996@live.com>
2024-03-26 01:05:56 +0100

    - helper ui components

---
+++ IMGLY/IMGLY/UI Module/Component/NavBarModifier.swift
@@ -0,0 +1,28 @@
+//
+//  NavBarModifier.swift
+//  SwiftHeroes
+//
+//  Created by macbook abdul on 25/03/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+	/// Sets background color and title color for UINavigationBar.
+	@available(iOS 14, *)
+	func navigationBar(backgroundColor: Color, titleColor: Color) -> some View {
+		let appearance = UINavigationBarAppearance()
+		appearance.configureWithTransparentBackground()
+		appearance.backgroundColor = UIColor(backgroundColor)
+
+		let uiTitleColor = UIColor(titleColor)
+		appearance.largeTitleTextAttributes = [.foregroundColor: uiTitleColor]
+		appearance.titleTextAttributes = [.foregroundColor: uiTitleColor]
+
+		UINavigationBar.appearance().standardAppearance = appearance
+		UINavigationBar.appearance().scrollEdgeAppearance = appearance
+
+		return self
+	}
+}
---
+++ IMGLY/IMGLY/UI Module/Component/RetryButton.swift
@@ -0,0 +1,37 @@
+//
+//  RetryButton.swift
+//  SwiftHeroes
+//
+//  Created by macbook abdul on 24/03/2024.
+//
+
+import Foundation
+import SwiftUI
+
+public struct RetryButton: View {
+	let text: String
+	let designLibrary: DesignLibraryProvider
+	let onTap: () -> Void
+
+	public var body: some View {
+		VStack {
+			Text(text)
+				.foregroundColor(designLibrary.color.text.standard)
+
+			Button(action: onTap) {
+				HStack {
+					Image("duck")
+						.renderingMode(.template)
+						.foregroundColor(designLibrary.color.icon.buttonPrimary)
+
+					Text("Retry...")
+						.foregroundColor(designLibrary.color.text.buttonPrimary)
+						.font(designLibrary.font.buttonFont.standard)
+				}
+				.frame(width: 150, height: 40)
+				.background(designLibrary.color.background.buttonPrimary)
+				.cornerRadius(designLibrary.miscellaneous.cornerRadius.button)
+			}
+		}
+	}
+}
