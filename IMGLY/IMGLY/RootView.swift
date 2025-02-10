013d185ce7af484569daa40e9ca0ce0ad425693a
abdul ahad <abdul_ahad1996@live.com>
2025-02-10 23:52:02 +0500

    format project

--- IMGLY/IMGLY/RootView.swift
+++ IMGLY/IMGLY/RootView.swift
@@ -22,7 +22,8 @@ public struct RootView: View {
   public var body: some View {
     if isActive {
       makeTreeFlowView(
-        designLibrary: isToggled ? DesignLibraryAlternative() : DesignLibrary(),
+        designLibrary: isToggled
+          ? DesignLibraryAlternative() : DesignLibrary(),
         loader: TreeNodeFactory.treeLoader(baseURL: baseURL))
     } else {
       SplashScreen(isActive: $isActive)
@@ -47,7 +48,9 @@ public struct RootView: View {
           Toggle(isOn: $isToggled) {
             Text("Change Theme")
           }.foregroundColor(designLibrary.color.text.standard)
-          .toggleStyle(SwitchToggleStyle(tint: designLibrary.color.background.buttonPrimary))
+          .toggleStyle(
+            SwitchToggleStyle(
+              tint: designLibrary.color.background.buttonPrimary))
       )
       .navigationDestination(for: TreeRoute.self) { route in
         switch route {
@@ -58,20 +61,26 @@ public struct RootView: View {
     }
   }
 
-  @ViewBuilder public func makeLeafView(id: String, designLibrary: DesignLibraryProvider)
+  @ViewBuilder public func makeLeafView(
+    id: String, designLibrary: DesignLibraryProvider
+  )
     -> some View
   {
     //remote loader
-    let remoteLeafNodeLoader = LeafFactory.remoteLeafNodeLoader(baseURL: baseURL, id: id)
+    let remoteLeafNodeLoader = LeafFactory.remoteLeafNodeLoader(
+      baseURL: baseURL, id: id)
 
     //local loader and local cache
     let localLeafNodeLoader = LeafFactory.localLeafNodeLoader()
 
     let leafNodeLoaderCacheDecorator = LeafNodeLoaderCacheDecorator(
-      remoteLeafNodeLoader: remoteLeafNodeLoader, leafNodeCache: localLeafNodeLoader)
+      remoteLeafNodeLoader: remoteLeafNodeLoader,
+      leafNodeCache: localLeafNodeLoader)
 
-    let leafNodeLoaderWithFallbackComposite = LeafNodeLoaderWithFallbackComposite(
-      primaryLoader: leafNodeLoaderCacheDecorator, secondaryLoader: localLeafNodeLoader)
+    let leafNodeLoaderWithFallbackComposite =
+      LeafNodeLoaderWithFallbackComposite(
+        primaryLoader: leafNodeLoaderCacheDecorator,
+        secondaryLoader: localLeafNodeLoader)
 
     let loader: LeafNodeLoader = leafNodeLoaderWithFallbackComposite
 
